7565393af3ac039bee5e0272c597fe237dbba892
EddKirich <161357628+EddKirich@users.noreply.github.com>
2024-03-03 20:30:34 +0300

    Delete user_input.py

--- user_input.py
+++
@@ -1,4 +0,0 @@
-name=input("What is your name? ")
-age=input("What is your age? ")
-location=input("Where are you located? ")
-print(f"Your name is {name}. You are {age} years old and live in {location}.")
