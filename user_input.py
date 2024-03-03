9ebaa71044802f0e55feee6e356544bf1f8b4076
EddKirich <161357628+EddKirich@users.noreply.github.com>
2024-03-03 20:43:45 +0300

    user_input.py

--- user_input.py
+++ user_input.py
@@ -1,4 +1,4 @@
-name=input("What is your name? ")
-age=input("What is your age? ")
-location=input("Where are you located? ")
+name=input("What is your name?")
+age=input("What is your age?")
+location=input("Where are you located?")
 print(f"Your name is {name}. You are {age} years old and live in {location}.")
